85d3774ead4816cfe0f3e68d13d2fa6577c1fc05
martin <martin.lopez@alumnos.uneatlantico.es>
2025-04-08 08:51:06 +0200

    a

--- CU_Ejercicio_7/api_mysql_php-main/ManageDB.php
+++ CU_Ejercicio_7/api_mysql_php-main/ManageDB.php
@@ -5,24 +5,24 @@ class ManageBD extends DB {
         $students = $this->connect()->query('SELECT * FROM student');
         $courses = $this->connect()->query('SELECT * FROM course');
         $departments = $this->connect()->query('SELECT * FROM department');
-        $end = $this->connect()->query('SELECT * FROM end');
+        //$end = $this->connect()->query('SELECT * FROM end');
         $instructor = $this->connect()->query('SELECT * FROM instructor');
-        $instructor_view = $this->connect()->query('SELECT * FROM instructor_view');
-        $profesor = $this->connect()->query('SELECT * FROM profesor');
+        //$instructor_view = $this->connect()->query('SELECT * FROM instructor_view');
+        //$profesor = $this->connect()->query('SELECT * FROM profesor');
         $takes = $this->connect()->query('SELECT * FROM takes');
-        $taylor_building = $this->connect()->query('SELECT * FROM taylor_building');
+        //$taylor_building = $this->connect()->query('SELECT * FROM taylor_building');
         $teaches = $this->connect()->query('SELECT * FROM teaches');
 
         $queries = array(
             "students" => $students,
             "courses" => $courses,
             "departments" => $departments,
-            "end" => $end,
+            //"end" => $end,
             "instructor" => $instructor,
-            "instructor_view" => $instructor_view,
-            "profesor" => $profesor,
+            //"instructor_view" => $instructor_view,
+            //"profesor" => $profesor,
             "takes" => $takes,
-            "taylor_building" => $taylor_building,
+            //"taylor_building" => $taylor_building,
             "teaches" => $teaches
         );
 
